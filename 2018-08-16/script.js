e31878df5f534d03450828d5ae989f161c32f9ad
xuanvien890 <xuanvien890@gmail.com>
2018-08-21 15:15:23 +0700

    2018-08-16

---
+++ 2018-08-16/script.js
@@ -0,0 +1,8 @@
+function batden(){
+    Var img = document.getElementById('js-img');
+    img.src = './pic_bulbon.gif';
+}
+function tatden(){
+    var img = document.getElementById('js-img');
+    img.src = './pic_bulboff.gif';
+}
